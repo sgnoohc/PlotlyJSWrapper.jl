ef30a4b075ed216d047ceb9d8c5a1132a2dc58d1
Philip Chang <philip@ucsd.edu>
2021-08-25 12:50:25 -0700

    update

--- src/PlotlyJSWrapper.jl
+++ src/PlotlyJSWrapper.jl
@@ -1,5 +1,19 @@
 module PlotlyJSWrapper
 
-greet() = print("Hello World!")
+using PlotlyJS
+
+export hist1d
+
+function hist1d(x, bins)
+    bins = StepRangeLen(bins)
+    lowedge = bins.ref.hi
+    step = bins.step.hi
+    highedge = lowedge + step * (bins.len-1)
+    histogram(x=x,
+              opacity=0.75,
+              xbins=attr(start=lowedge, size=step),
+              xbins_end=highedge
+             )
+end
 
 end # module
