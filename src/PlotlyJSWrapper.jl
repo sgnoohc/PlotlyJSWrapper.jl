d4e7eee85dd5bd83e28f628f31b3712e99bf7556
Philip Chang <philip@ucsd.edu>
2021-08-25 13:01:18 -0700

    update

--- src/PlotlyJSWrapper.jl
+++ src/PlotlyJSWrapper.jl
@@ -6,8 +6,8 @@ export hist1d
 
 function hist1d(x, bins)
     bins = StepRangeLen(bins)
-    lowedge = bins.ref.hi
-    step = bins.step.hi
+    lowedge = Float64(bins.ref)
+    step = Float64(bins.step)
     highedge = lowedge + step * (bins.len-1)
     histogram(x=x,
               opacity=0.75,
