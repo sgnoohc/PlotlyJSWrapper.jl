44a103769385b6e823635b4aafb4bbd997897e71
Philip Chang <philip@ucsd.edu>
2021-08-27 14:25:17 -0700

    update

--- examples/example2/example2.jl
+++ examples/example2/example2.jl
@@ -2,15 +2,15 @@ using PlotlyJSWrapper
 using FHist
 
 # Creating example histograms
-h1 = Hist1D(randn(3000),-5:0.5:5)
-h2 = Hist1D(randn(1000).+1,-5:0.5:5)
-h3 = Hist1D((randn(1000).+2)./2,-5:0.5:5)
-h4 = Hist1D((randn(2000).-2).*2,-5:0.5:5)
-h5 = Hist1D(randn(2000).*5,-5:0.5:5)
-h6 = Hist1D(randn(2000).-0.5,-5:0.5:5)
-data = Hist1D(randn(9000),-5:0.5:5)
-data2 = Hist1D((randn(8000).-1).*1.5,-5:0.5:5)
-signal = Hist1D((randn(1000).+10)./3,-5:0.5:5)
+h1 = Hist1D(randn(3000),-5:0.5:5) * 0.1
+h2 = Hist1D(randn(1000).+1,-5:0.5:5) * 0.1
+h3 = Hist1D((randn(1000).+2)./2,-5:0.5:5) * 0.1
+h4 = Hist1D((randn(2000).-2).*2,-5:0.5:5) * 0.1
+h5 = Hist1D(randn(2000).*5,-5:0.5:5) * 0.1
+h6 = Hist1D(randn(2000).-0.5,-5:0.5:5) * 0.1
+data = Hist1D(randn(900),-5:0.5:5) + Hist1D(randn(200).-3.5,-5:0.5:5) + Hist1D(randn(45).+4,-5:0.5:5)
+data2 = Hist1D(randn(900).+1,-5:0.5:5) + Hist1D(randn(150).-3.5,-5:0.5:5) + Hist1D(randn(45).+3,-5:0.5:5)
+signal = Hist1D((randn(1000).+10)./3,-5:0.5:5) * 0.1
 
 # Plotting
 plot_stack(
