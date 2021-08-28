5c766d7b987e1b0d3ad2f0ffeae5906ec1e5b45b
Documenter.jl <documenter@juliadocs.github.io>
2021-08-28 00:55:28 +0000

    build based on 7ed434c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/","page":"APIs","title":"APIs","text":"Modules = [PlotlyJSWrapper]","category":"page"},{"location":"#PlotlyJSWrapper.jl","page":"Introduction","title":"PlotlyJSWrapper.jl","text":"","category":"section"}]
+}
