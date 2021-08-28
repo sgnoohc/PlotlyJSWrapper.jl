67bbd1d308c22f4d0b9ff4fba781577dc662f494
Philip Chang <philip@ucsd.edu>
2021-08-27 17:21:26 -0700

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,21 @@
 using Documenter, PlotlyJSWrapper
 
-makedocs(sitename="PlotlyJSWrapper Documentation")
+makedocs(
+         modules=[PlotlyJSWrapper],
+         format = Documenter.HTML(
+                                  prettyurls = get(ENV, "CI", nothing) == "true",
+                                  # assets = ["assets/logo.ico"],
+                                 ),
+         pages=[
+                "Introduction" => "index.md",
+                "APIs" => "api.md",
+               ],
+         repo="https://github.com/sgnoohc/PlotlyJSWrapper.jl/blob/{commit}{path}#L{line}",
+         sitename="PlotlyJSWrapper.jl",
+         authors="Philip Chang",
+         assets=String[],
+        )
+
+deploydocs(
+          repo = "github.com/sgnoohc/PlotlyJSWrapper.jl",
+          )
